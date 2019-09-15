a08e773b52786e82329ee8e584ada57a65da0c9c
Soya Beans <5205954@student.gloscol.ac.uk>
2019-09-15 22:08:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+const DEVELOPER = 1;
+
+let mainWindow;
+let secondWindow;
+let thirdWindow;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol:'file',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function Second(){
+    secondWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Second Window'
+    });
+    secondWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'secondWindow.html'),
+        protocol:'file',
+        slashes: true
+    }));
+    secondWindow.on('close', function(){
+        secondWindow = null;
+    });
+}
+
+function Third(){
+    thirdWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Third Window'
+    });
+    thirdWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'thirdWindow.html'),
+        protocol:'file',
+        slashes: true
+    }));
+    thirdWindow.on('close', function(){
+        thirdWindow = null;
+    });
+}
+
+const mainMenuTemplate = [
+    {
+        label:'Events',
+        submenu: [
+            {
+                label: 'Second',
+                click(){
+                    Second();
+                }
+            },
+            {
+                label: 'Third',
+                click(){
+                    Third();
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwain' ? 'Command+Q' :
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if (process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+if (DEVELOPER !== 0){
+    mainMenuTemplate.push({
+        label: 'Dev Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwain' ? 'Command+I' :
+                'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
